a7c4006b594802f1c71d8d1a272ac361711632f5
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-08-07 19:47:50 +0530

    Update waf.ts

--- netlify/edge-functions/waf.ts
+++ netlify/edge-functions/waf.ts
@@ -1,7 +1,6 @@
 import type {Config} from '@netlify/edge-functions'
 
 export default async function(req : Request) {
-  console.log(req.headers.get('x-nf-waf-bypass-token'))
   const res = await fetch('https://zd-288344.netlify.app/api/geo', {
     headers: {
       'x-nf-waf-bypass-token': req.headers.get('x-nf-waf-bypass-token')
